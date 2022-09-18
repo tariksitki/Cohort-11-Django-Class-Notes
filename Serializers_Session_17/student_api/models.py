99d32d2a509ff48fb7429be67ecf575740ce8327
tarik <63872091+tariksitki@users.noreply.github.com>
2022-09-18 03:17:58 +0200

    serializers addded

--- Serializers_Session_17/student_api/models.py
+++ Serializers_Session_17/student_api/models.py
@@ -27,7 +27,7 @@ class Path(models.Model):
     path_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.path_name}"    
+        return f"{self.path_name}"      
 
 class Student(models.Model):
     path = models.ForeignKey(Path, related_name='students', on_delete=models.CASCADE)
